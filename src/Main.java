c81b98330e75846f0f13d17f26aefe1415f8f497
alvaroiglesias93 <alvaroiglesias1993@outlook.es>
2022-11-21 17:26:45 +0100

    añadiendo código para probar las propiedades de Java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,47 @@
-public class Main {
+import java.util.Scanner;
+class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        int inicio;
+        int fin;
+        int contador = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("TEXTO INNECESARIO");
+
+        System.out.println("Escribe el inicio:");
+        inicio = sc.nextInt();
+
+        System.out.println("Escribe el fin:");
+        fin = sc.nextInt();
+
+
+
+        for (int x = inicio; x <= fin; x++) {
+            if (esPrimo(x)) {
+                contador++;
+                System.out.print(x + ",");
+            }
+        }
+        System.out.printf("\nTotal: %d", contador);
+        sc.close();
     }
+
+
+
+    public static boolean esPrimo(int numero) {
+        // El 0, 1 y 4 no son primos
+        if (numero == 0 || numero == 1 || numero == 4) {
+            return false;
+        }
+
+        for (int x = 2; x < numero / 2; x++) {
+            // Si es divisible por cualquiera de estos números, no es primo
+            if (numero % x == 0)
+                return false;
+        }
+        return true;
+    }
+
 }
